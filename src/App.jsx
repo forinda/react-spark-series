e96725991432750011ba7542c9088d642132d4e1
Felix Orinda <forinda82@gmail.com>
2023-04-04 22:31:11 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const App = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>Level 1</h1>
+      </div>
+      <div>
+        <h1>Level 2</h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
