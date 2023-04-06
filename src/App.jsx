95fb93354ab99392f35b002ab414857f5729cc8b
Felix Orinda <forinda82@gmail.com>
2023-04-07 00:31:16 +0300

    React css card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import React, { Fragment } from "react";
+import React from "react";
+import "./App.css"
 
 const App = () => {
+  // const imageLink = "https://v1.tailwindcss.com/img/card-top.jpg"
   return (
-    <Fragment>
-      <div>
-        <h1>Level 1</h1>
+    <div className="page-wrapper">
+      <div className="card-component">
+        <img
+          src="https://v1.tailwindcss.com/img/card-top.jpg"
+          alt=""
+          className="card-image"
+        />
+        <div className="card-content">
+          <h1>The coldest sunset</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            magnam! Consequatur soluta accusamus dolores reprehenderit similique
+            error quae quasi quibusdam eum impedit quas est aut, magnam dolorum
+            in saepe harum?
+          </p>
+          <div className="tags">
+            <span className="tag">#photograpy</span>
+            <span className="tag">#travel</span>
+            <span className="tag">#winter</span>
+          </div>
+        </div>
       </div>
-      <div>
-        <h1>Level 2</h1>
-      </div>
-    </Fragment>
+    </div>
   );
 };
 
